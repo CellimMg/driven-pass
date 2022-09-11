4ab38e46c44f3fd73031418194d75b2b319849c5
Marcelo Luciano <cello.ma16@gmail.com>
2022-09-11 13:32:41 -0300

    [default] teste

--- src/schemas/signin_schema.ts
+++ src/schemas/signin_schema.ts
@@ -5,6 +5,10 @@ import { UserInsert } from "../types/User";
 const signinSchema = joi.object<UserInsert>({
     email: joi.string().email().required(),
     password: joi.string().required() //Por questões de segurança, não informa a quantidade de caracteres
-})
+});
+
+
+
+//comentario de teste
 
 export default signinSchema;
